4fb652eef9f223beb402e587a59a5958a01089ad
electimon <electimon@gmail.com>
2022-01-31 14:02:35 +0000

    common: Create common.mk
    Co-authored-by: Vishalcj17 <vishalcj@aospa.co>
    Co-authored-by: nullbytepl <nullbytepl@gmail.com>
    Co-authored-by: xboxfanj <jake@aospa.co>
    Co-authored-by: ZVNexus <akoskovich@pm.me>
    
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ common.mk
@@ -0,0 +1,23 @@
+QCOM_COMMON_PATH := device/qcom/common
+
+ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
+# Compatibility matrix
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+endif
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(QCOM_COMMON_PATH)/system/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(QCOM_COMMON_PATH)/system/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(QCOM_COMMON_PATH)/system/public.libraries.system_ext-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+
+# SECCOMP Extensions
+PRODUCT_COPY_FILES += \
+    $(QCOM_COMMON_PATH)/system/seccomp/codec2.software.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.software.ext.policy \
+    $(QCOM_COMMON_PATH)/system/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
+    $(QCOM_COMMON_PATH)/system/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(QCOM_COMMON_PATH)/system/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
