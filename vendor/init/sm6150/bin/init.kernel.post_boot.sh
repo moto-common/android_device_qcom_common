87f39e6f7a8decfdf1e0a431d00daa0de42eec38
electimon <electimon@gmail.com>
2022-11-17 02:55:17 +0000

    common: vendor/init: Add post boot script for sm6150
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ vendor/init/sm6150/bin/init.kernel.post_boot.sh
@@ -0,0 +1,245 @@
+#=============================================================================
+# Copyright (c) 2020 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Qualcomm Technologies, Inc.
+#
+# Copyright (c) 2012-2013, 2016-2020, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+
+function configure_zram_parameters() {
+	MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+	MemTotal=${MemTotalStr:16:8}
+
+	# Zram disk - 75% for < 2GB devices .
+	# For >2GB devices, size = 50% of RAM size. Limit the size to 4GB.
+
+	let RamSizeGB="( $MemTotal / 1048576 ) + 1"
+	diskSizeUnit=M
+	if [ $RamSizeGB -le 2 ]; then
+		let zRamSizeMB="( $RamSizeGB * 1024 ) * 3 / 4"
+	else
+		let zRamSizeMB="( $RamSizeGB * 1024 ) / 2"
+	fi
+
+	# use MB avoid 32 bit overflow
+	if [ $zRamSizeMB -gt 4096 ]; then
+		let zRamSizeMB=4096
+	fi
+
+	echo lz4 > /sys/block/zram0/comp_algorithm
+
+	if [ -f /sys/block/zram0/disksize ]; then
+		if [ -f /sys/block/zram0/use_dedup ]; then
+			echo 1 > /sys/block/zram0/use_dedup
+		fi
+		echo "$zRamSizeMB""$diskSizeUnit" > /sys/block/zram0/disksize
+
+		# ZRAM may use more memory than it saves if SLAB_STORE_USER
+		# debug option is enabled.
+		if [ -e /sys/kernel/slab/zs_handle ]; then
+			echo 0 > /sys/kernel/slab/zs_handle/store_user
+		fi
+		if [ -e /sys/kernel/slab/zspage ]; then
+			echo 0 > /sys/kernel/slab/zspage/store_user
+		fi
+
+		mkswap /dev/block/zram0
+		swapon /dev/block/zram0 -p 32758
+	fi
+}
+
+function configure_read_ahead_kb_values() {
+	MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+	MemTotal=${MemTotalStr:16:8}
+
+	dmpts=$(ls /sys/block/*/queue/read_ahead_kb | grep -e dm -e mmc)
+
+	# Set 128 for <= 3GB &
+	# set 512 for >= 4GB targets.
+	if [ $MemTotal -le 3145728 ]; then
+		ra_kb=128
+	else
+		ra_kb=512
+	fi
+	if [ -f /sys/block/mmcblk0/bdi/read_ahead_kb ]; then
+		echo $ra_kb > /sys/block/mmcblk0/bdi/read_ahead_kb
+	fi
+	if [ -f /sys/block/mmcblk0rpmb/bdi/read_ahead_kb ]; then
+		echo $ra_kb > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
+	fi
+	for dm in $dmpts; do
+		echo $ra_kb > $dm
+	done
+}
+
+function configure_memory_parameters() {
+	# Set Memory parameters.
+
+	# Set swappiness to 100 for all targets
+	echo 100 > /proc/sys/vm/swappiness
+
+	# Disable wsf for all targets beacause we are using efk.
+	# wsf Range : 1..1000 So set to bare minimum value 1.
+	echo 1 > /proc/sys/vm/watermark_scale_factor
+	configure_zram_parameters
+	configure_read_ahead_kb_values
+	echo 0 > /proc/sys/vm/page-cluster
+
+	#Spawn 2 kswapd threads which can help in fast reclaiming of pages
+	echo 2 > /proc/sys/vm/kswapd_threads
+}
+
+# Core control parameters on silver
+echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
+echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
+echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
+echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
+echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
+echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
+echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
+echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
+
+
+# Setting b.L scheduler parameters
+# default sched up and down migrate values are 90 and 85
+echo 65 > /proc/sys/kernel/sched_downmigrate
+echo 71 > /proc/sys/kernel/sched_upmigrate
+# default sched up and down migrate values are 100 and 95
+echo 85 > /proc/sys/kernel/sched_group_downmigrate
+echo 100 > /proc/sys/kernel/sched_group_upmigrate
+echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
+
+# colocation v3 settings
+echo 740000 > /proc/sys/kernel/sched_little_cluster_coloc_fmin_khz
+
+
+# configure governor settings for little cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+echo 1209600 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
+if [ $sku_identified != 1 ]; then
+	echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+fi
+
+# configure governor settings for big cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
+echo 0 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
+echo 0 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/down_rate_limit_us
+echo 1209600 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
+if [ $sku_identified != 1 ]; then
+	echo 768000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
+fi
+
+# sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
+echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
+echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
+echo 85 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
+
+echo "0:1209600" > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
+
+# Set Memory parameters
+configure_memory_parameters
+
+# Enable bus-dcvs
+for device in /sys/devices/platform/soc
+do
+	for cpubw in $device/*cpu-cpu-llcc-bw/devfreq/*cpu-cpu-llcc-bw
+	do
+		echo "bw_hwmon" > $cpubw/governor
+		cat $cpubw/available_frequencies | cut -d " " -f 1 > $cpubw/min_freq
+		echo "2288 4577 7110 9155 12298 14236" > $cpubw/bw_hwmon/mbps_zones
+		echo 4 > $cpubw/bw_hwmon/sample_ms
+		echo 68 > $cpubw/bw_hwmon/io_percent
+		echo 20 > $cpubw/bw_hwmon/hist_memory
+		echo 0 > $cpubw/bw_hwmon/hyst_length
+		echo 80 > $cpubw/bw_hwmon/down_thres
+		echo 0 > $cpubw/bw_hwmon/guard_band_mbps
+		echo 250 > $cpubw/bw_hwmon/up_scale
+		echo 1600 > $cpubw/bw_hwmon/idle_mbps
+		echo 50 > $cpubw/polling_interval
+	done
+
+	for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
+	do
+		echo "bw_hwmon" > $llccbw/governor
+		cat $llccbw/available_frequencies | cut -d " " -f 1 > $llccbw/min_freq
+		echo "1144 1720 2086 2929 3879 5931 6881" > $llccbw/bw_hwmon/mbps_zones
+		echo 4 > $llccbw/bw_hwmon/sample_ms
+		echo 68 > $llccbw/bw_hwmon/io_percent
+		echo 20 > $llccbw/bw_hwmon/hist_memory
+		echo 0 > $llccbw/bw_hwmon/hyst_length
+		echo 80 > $llccbw/bw_hwmon/down_thres
+		echo 0 > $llccbw/bw_hwmon/guard_band_mbps
+		echo 250 > $llccbw/bw_hwmon/up_scale
+		echo 1600 > $llccbw/bw_hwmon/idle_mbps
+		echo 40 > $llccbw/polling_interval
+	done
+
+	#Enable mem_latency governor for L3, LLCC, and DDR scaling
+	for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
+	do
+		echo "mem_latency" > $memlat/governor
+		cat $memlat/available_frequencies | cut -d " " -f 1 > $memlat/min_freq
+	done
+
+	#Enable compute governor for gold latfloor
+	for latfloor in $device/*cpu-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
+	do
+		echo "compute" > $latfoor/governor
+		cat $latfloor/available_frequencies | cut -d " " -f 1 > $latfloor/min_freq
+	done
+
+	#Gold L3 ratio ceil
+	for l3silver in $device/*cpu0-cpu-l3-lat/devfreq/*cpu0-cpu-l3-lat
+	do
+		echo "mem_latency" > $l3silver/governor
+		cat $l3silver/available_frequencies | cut -d " " -f 1 > $l3silver/min_freq
+	done
+
+	#Gold L3 ratio ceil
+	for l3gold in $device/*cpu6-cpu-l3-lat/devfreq/*cpu6-cpu-l3-lat
+	do
+		echo "mem_latency" > $l3gold/governor
+		cat $l3gold/available_frequencies | cut -d " " -f 1 > $l3gold/min_freq
+	done
+done
+
+# Turn off scheduler boost at the end
+echo 0 > /proc/sys/kernel/sched_boost
+
+# Turn on sleep modes.
+echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+
+Log kernel wake-up source and clock info
+echo 1 > /sys/module/msm_show_resume_irq/parameters/debug_mask
+echo 1 > /sys/kernel/debug/clk/debug_suspend
+
+setprop vendor.dcvs.prop 1
+setprop vendor.post_boot.parsed 1
